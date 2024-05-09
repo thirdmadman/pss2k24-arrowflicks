66ca5e1b9219e86b24aafe8cb2e7e7be5b7a89fa
thirdmadman <thirdmadman@gmail.com>
2024-05-09 17:39:31 +0200

    feat: add icons components

---
+++ src/app/components/shared/icons/IconArrowDown.tsx
@@ -0,0 +1,12 @@
+export function IconArrowDown(color: string) {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M3.33325 6L7.47929 9.55374C7.77888 9.81054 8.22096 9.81054 8.52055 9.55374L12.6666 6"
+        stroke={color}
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
---
+++ src/app/components/shared/icons/IconClose.tsx
@@ -0,0 +1,15 @@
+export function IconClose(color: string) {
+  return (
+    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_18005_1868)">
+        <path stroke={color} d="M12 4L4 12" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+        <path stroke={color} d="M4 4L12 12" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+      </g>
+      <defs>
+        <clipPath id="clip0_18005_1868">
+          <rect width="16" height="16" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
