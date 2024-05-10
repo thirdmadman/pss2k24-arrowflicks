0a6cc5b32800349f0358da7a7f8f18b3b425a559
thirdmadman <thirdmadman@gmail.com>
2024-05-10 21:47:10 +0200

    feat: update search bar submit action handler

--- src/app/components/shared/button/PrimaryButton.tsx
+++ src/app/components/shared/button/PrimaryButton.tsx
@@ -4,15 +4,17 @@ import classes from './PrimaryButton.module.css';
 interface IPrimaryButtonProps {
   text: string;
   size: 'small' | 'medium';
+  onClickEvent: () => void;
 }
 
 export function PrimaryButton(props: IPrimaryButtonProps) {
-  const { text, size } = props;
+  const { text, size, onClickEvent } = props;
   return (
     <UnstyledButton
       classNames={{
         root: `${classes.root} ${size === 'small' ? classes.small : classes.medium}`,
       }}
+      onClick={() => onClickEvent()}
     >
       {text}
     </UnstyledButton>
--- src/app/components/shared/icons/home/SearchBar.tsx
+++ src/app/components/shared/icons/home/SearchBar.tsx
@@ -1,14 +1,36 @@
+'use client';
+
 import { getColor } from '@/theme/theme';
 import { TextInput } from '@mantine/core';
 import { IconSearch } from '../IconSearch';
 import classes from './SearchBar.module.css';
 import { PrimaryButton } from '../../button/PrimaryButton';
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { setQueryParam } from '@/utils/setQueryParam';
 
 interface ISearchBarProps {
   text: string;
 }
 
 export function SearchBar(props: ISearchBarProps) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const updateSearch = (searchQuery: string, isReload = false) => {
+    const current = new URLSearchParams(Array.from(searchParams.entries()));
+    setQueryParam(current, 'query', searchQuery);
+    const search = current.toString();
+    const query = search ? `?${search}` : '';
+    if (isReload) {
+      router.push(`${pathname}${query}`, { scroll: false });
+      return;
+    }
+    router.replace(`${pathname}${query}`, { scroll: false });
+  };
+
   const { text } = props;
 
   const grayColor = getColor('gray', 6);
@@ -22,8 +44,11 @@ export function SearchBar(props: ISearchBarProps) {
       leftSectionPointerEvents="none"
       leftSection={icon}
       rightSectionWidth={88}
-      rightSection={<PrimaryButton text="Search" size="small" />}
+      rightSection={<PrimaryButton text="Search" size="small" onClickEvent={() => updateSearch(searchQuery, true)} />}
       defaultValue={text}
+      onChange={(e) => {
+        setSearchQuery(e.currentTarget.value);
+      }}
       aria-label="Search movie title"
       placeholder="Search movie title"
       classNames={{
