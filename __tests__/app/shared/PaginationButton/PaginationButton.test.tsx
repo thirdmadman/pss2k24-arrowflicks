20feae634b674b232da5d6d3af29680fb5812b7c
thirdmadman <thirdmadman@gmail.com>
2024-05-29 20:02:42 +0200

    feat: add PaginationButton test

---
+++ __tests__/app/shared/PaginationButton/PaginationButton.test.tsx
@@ -0,0 +1,15 @@
+import { screen } from '@testing-library/react';
+import { render } from '../../../test-utils/render';
+import { PaginationButton } from '@/app/components/shared/PaginationButton/PaginationButton';
+
+describe('PaginationButton component', () => {
+  it('should render without failing', () => {
+    const { container } = render(<PaginationButton href={''} />);
+    expect(container.firstElementChild).not.toBeNull();
+  });
+
+  it('should contain given text', () => {
+    render(<PaginationButton href={''} text="PaginationButtonText" />);
+    expect(screen.getByText('PaginationButtonText')).not.toBeNull();
+  });
+});
