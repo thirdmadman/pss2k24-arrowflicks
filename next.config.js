77bf327322e81e3a61b411eccab4dad712e22dfc
thirdmadman <thirdmadman@gmail.com>
2024-06-01 00:55:50 +0200

    feat: add remotePatterns

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.tmdb.org',
+        port: '',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
