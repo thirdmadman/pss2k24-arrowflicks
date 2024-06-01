300f8f6cfb1a799f05964bab001b45748c08e65f
thirdmadman <thirdmadman@gmail.com>
2024-06-01 17:24:27 +0200

    feat: add MoviesPaginatedList

---
+++ src/app/components/shared/MoviesPaginatedList/MoviesPaginatedList.tsx
@@ -0,0 +1,60 @@
+import { Flex, Stack } from '@mantine/core';
+import { MovieCard } from '@/app/components/shared/MovieCard/MovieCard';
+import { getMovieDiscoveryPaginatedList } from '@/lib/utils/getMovieDiscoveryPaginatedList';
+import { IMAGES_POSTERS_BASE } from '@/constants';
+import { Pagination } from '@/app/components/shared/Pagination/Pagination';
+
+export async function MoviesPaginatedList({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    ratingFrom?: string;
+    ratingTo?: string;
+    page?: string;
+    sortBy?: string;
+    releaseYear?: string;
+    genres?: string;
+  };
+}) {
+  const ratingFrom = searchParams?.ratingFrom ?? '';
+  const ratingTo = searchParams?.ratingTo ?? '';
+  const sortBy = searchParams?.sortBy ?? '';
+  const releaseYear = searchParams?.releaseYear ?? '';
+  const page = searchParams?.page ? parseInt(searchParams.page) : 1;
+  const genres = searchParams?.genres;
+
+  const moviesList = await getMovieDiscoveryPaginatedList(
+    { ratingFrom, ratingTo, sortBy, releaseYear, genres },
+    { page: page.toString() }
+  );
+
+  return (
+    <Stack>
+      <Flex direction="row" justify="space-between" align="flex-start" wrap="wrap" gap="16px">
+        {moviesList.results.map((el) => (
+          <MovieCard
+            movieId={el.id.toString()}
+            image={{
+              src: el.poster_path ? `${IMAGES_POSTERS_BASE}/w500${el.poster_path}` : undefined,
+              alt: el.title,
+            }}
+            title={el.title}
+            year={parseInt(el.release_date.split('-')[0], 10)}
+            rating={el.vote_average}
+            reviewsCount={el.vote_count}
+            genres={['Comedy', 'Horror', 'Drama']}
+            key={el.id}
+          />
+        ))}
+      </Flex>
+
+      <Pagination
+        searchParams={searchParams}
+        totalItemsCount={moviesList.total_results}
+        currentPage={page}
+        itemsPerPage={20}
+      />
+    </Stack>
+  );
+}
