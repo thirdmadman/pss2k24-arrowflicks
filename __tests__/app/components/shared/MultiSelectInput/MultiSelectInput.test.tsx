099b1f2afd31560394172c39dfd9f5438677e30b
thirdmadman <thirdmadman@gmail.com>
2024-06-01 18:25:21 +0200

    fix: correct path fix: update test

--- __tests__/app/components/shared/MultiSelectInput/MultiSelectInput.test.tsx
+++ __tests__/app/components/shared/MultiSelectInput/MultiSelectInput.test.tsx
@@ -1,14 +1,40 @@
 import { screen } from '@testing-library/react';
-import { render } from '../../../test-utils/render';
+import { render } from '../../../../test-utils/render';
 import { MultiSelectInput } from '@/app/components/shared/MultiSelectInput/MultiSelectInput';
 
 describe('MultiSelectInput component', () => {
+  beforeAll(() => {
+    vi.mock('next/navigation', async () => {
+      const actual = await vi.importActual('next/navigation');
+      return {
+        ...actual,
+        useRouter() {
+          return {
+            route: '/',
+            pathname: '',
+            query: '',
+            asPath: '',
+          };
+        },
+        usePathname: () => '',
+        useSearchParams() {
+          return {
+            get(value: string) {
+              return value;
+            },
+          };
+        },
+      };
+    });
+  });
+
   it('should render without failing', () => {
     const { container } = render(
       <MultiSelectInput
         label={'MultiSelectInputLabel'}
         placeholder={'MultiSelectInputPlaceholder'}
         options={['1', '2']}
+        queryKey="queryKey"
       />
     );
     expect(container.firstElementChild).not.toBeNull();
@@ -20,6 +46,7 @@ describe('MultiSelectInput component', () => {
         label={'MultiSelectInputLabel'}
         placeholder={'MultiSelectInputPlaceholder'}
         options={['1', '12345678']}
+        queryKey="queryKey"
       />
     );
     expect(screen.getByText('MultiSelectInputLabel')).not.toBeNull();
@@ -31,6 +58,7 @@ describe('MultiSelectInput component', () => {
         label={'MultiSelectInputLabel'}
         placeholder={'MultiSelectInputPlaceholder'}
         options={['1', '12345678']}
+        queryKey="queryKey"
       />
     );
     expect(screen.getByText('12345678')).not.toBeNull();
