288cdc13dd30ade5e7374a581944384294faff72
thirdmadman <thirdmadman@gmail.com>
2024-06-01 22:33:47 +0200

    feat: add SelectInput test

---
+++ __tests__/app/components/shared/SelectInput/SelectInput.test.tsx
@@ -0,0 +1,69 @@
+import { screen } from '@testing-library/react';
+import { render } from '../../../../test-utils/render';
+import { SelectInput } from '@/app/components/shared/SelectInput/SelectInput';
+
+describe('SelectInput component', () => {
+  beforeAll(() => {
+    vi.mock('next/navigation', async () => {
+      const actual = await vi.importActual('next/navigation');
+      return {
+        ...actual,
+        useRouter() {
+          return {
+            route: '/',
+            pathname: '',
+            query: '',
+            asPath: '',
+          };
+        },
+        usePathname: () => '',
+        useSearchParams() {
+          return {
+            get(value: string) {
+              return value;
+            },
+          };
+        },
+      };
+    });
+  });
+
+  it('should render without failing', () => {
+    const { container } = render(
+      <SelectInput
+        label={'MultiSelectInputLabel'}
+        placeholder={'MultiSelectInputPlaceholder'}
+        options={['1', '2']}
+        queryKey="queryKey"
+        allowDeselect
+      />
+    );
+    expect(container.firstElementChild).not.toBeNull();
+  });
+
+  it('should contain given label', () => {
+    render(
+      <SelectInput
+        label={'MultiSelectInputLabel'}
+        placeholder={'MultiSelectInputPlaceholder'}
+        options={['1', '12345678']}
+        queryKey="queryKey"
+        allowDeselect
+      />
+    );
+    expect(screen.getByText('MultiSelectInputLabel')).not.toBeNull();
+  });
+
+  it('should contain given values in option', () => {
+    render(
+      <SelectInput
+        label={'MultiSelectInputLabel'}
+        placeholder={'MultiSelectInputPlaceholder'}
+        options={['1', '12345678']}
+        queryKey="queryKey"
+        allowDeselect
+      />
+    );
+    expect(screen.getByText('12345678')).not.toBeNull();
+  });
+});
