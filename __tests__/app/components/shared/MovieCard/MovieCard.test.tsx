b064cfa47ab5e2d164a070c22cb290655ac43133
thirdmadman <thirdmadman@gmail.com>
2024-06-01 18:23:12 +0200

    fix: correct path fix: update test

--- __tests__/app/components/shared/MovieCard/MovieCard.test.tsx
+++ __tests__/app/components/shared/MovieCard/MovieCard.test.tsx
@@ -1,21 +1,21 @@
 import { screen } from '@testing-library/react';
-import { render } from '../../../test-utils/render';
+import { render } from '../../../../test-utils/render';
 import { MovieCard } from '@/app/components/shared/MovieCard/MovieCard';
 
 describe('MovieCard component', () => {
   it('should render without failing', () => {
     const { container } = render(
       <MovieCard
+        movieId={''}
         image={{
           src: undefined,
           alt: '',
         }}
-        title={undefined}
-        year={undefined}
-        rating={undefined}
-        userRating={undefined}
-        reviewsCount={undefined}
-        genres={undefined}
+        title={'MovieCardTitle'}
+        year={1901}
+        rating={9.8}
+        reviewsCount={1000}
+        genres={['genre1', 'genre2']}
       />
     );
     expect(container.firstElementChild).not.toBeNull();
@@ -24,6 +24,7 @@ describe('MovieCard component', () => {
   it('should contain reviews number and rating', () => {
     render(
       <MovieCard
+        movieId={''}
         image={{
           src: undefined,
           alt: '',
@@ -31,14 +32,12 @@ describe('MovieCard component', () => {
         title={'MovieCardTitle'}
         year={1901}
         rating={9.8}
-        userRating={9}
         reviewsCount={1000}
         genres={['genre1', 'genre2']}
       />
     );
 
     expect(screen.getByText('MovieCardTitle')).not.toBeNull();
-    expect(screen.getByText('9')).not.toBeNull();
     expect(screen.getByText('1901')).not.toBeNull();
     expect(screen.getByText('(1K)')).not.toBeNull();
     expect(screen.getByText('9.8')).not.toBeNull();
