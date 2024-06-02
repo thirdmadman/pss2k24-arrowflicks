19d654e9bf89ce28ee6ff769528808656507687a
thirdmadman <thirdmadman@gmail.com>
2024-06-03 00:27:10 +0200

    fix: correct layout height

--- src/app/components/shared/PageLayout/PageLayout.tsx
+++ src/app/components/shared/PageLayout/PageLayout.tsx
@@ -13,7 +13,7 @@ interface IPageLayoutProps {
 
 export function PageLayout(props: IPageLayoutProps) {
   return (
-    <Flex wrap="nowrap" gap={0}>
+    <Flex wrap="nowrap" gap={0} mih="100%">
       <Flex direction="column" bg="purple.1" maw="280px" p="24px" gap="60px" w="100%">
         <Logo />
         <Stack>
