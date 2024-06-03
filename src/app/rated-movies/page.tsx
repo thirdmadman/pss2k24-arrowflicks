29dbafe33ae86b1cc62acff3de4cee70c23969cd
thirdmadman <thirdmadman@gmail.com>
2024-06-03 22:29:45 +0200

    feat: update layout feat: add basic page

---
+++ src/app/rated-movies/page.tsx
@@ -0,0 +1,28 @@
+import { Container, Group, Stack, Title } from '@mantine/core';
+import { Metadata } from 'next';
+import { PageLayout } from '@/app/components/shared/PageLayout/PageLayout';
+
+export const metadata: Metadata = {
+  title: 'ArrowFlicks - Rated movies',
+  description: 'Page where are stored all your rated movies (only locally)',
+};
+
+export default function Home(props: { searchParams: { [key: string]: string } }) {
+  const { searchParams } = props;
+  return (
+    <>
+      <PageLayout>
+        <Container w="100%" size="1160px" c="black" py="40px" px="90px" bg="grey.2">
+          <Stack gap="40px">
+            <Group justify="space-between" wrap="nowrap" gap="40px">
+              <Title size="32px" lh="45px" c="black" order={1} fw="bold">
+                Rated movies
+              </Title>
+            </Group>
+            <Group>{searchParams.lo}</Group>
+          </Stack>
+        </Container>
+      </PageLayout>
+    </>
+  );
+}
