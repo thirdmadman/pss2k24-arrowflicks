93a0e3bd77c71b7a208594335bbccd79178ec9e4
thirdmadman <thirdmadman@gmail.com>
2024-06-23 17:43:48 +0200

    feat: add MoviesPaginatedListSkeleton

---
+++ src/app/components/home/MoviesPaginatedList/MoviesPaginatedListSkeleton.tsx
@@ -0,0 +1,18 @@
+import { Flex, Skeleton, Stack } from '@mantine/core';
+import { MovieCardSkeleton } from '../../shared/MovieCard/MovieCardSkeleton';
+
+const array = new Array(20).fill(1).map((el, i) => i + 1);
+
+export function MoviesPaginatedListSkeleton() {
+  return (
+    <Stack>
+      <Flex direction="row" justify="space-between" align="flex-start" wrap="wrap" gap="16px">
+        {array.map((el) => (
+          <MovieCardSkeleton key={el} />
+        ))}
+      </Flex>
+
+      <Skeleton height={30} maw={300} />
+    </Stack>
+  );
+}
