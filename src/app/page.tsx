fdbe8c772881ac9ac89560eb30177cc09d9f7b3d
thirdmadman <thirdmadman@gmail.com>
2024-06-24 11:16:04 +0200

    feat: impl use of SuspenseAfterMounted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { Suspense } from 'react';
 import { MoviesPaginatedList } from './components/home/MoviesPaginatedList/MoviesPaginatedList';
 import { generateSuspenseKeyBySearchParams } from '@/lib/utils/generateSuspenseKeyBySearchParams';
 import { MoviesPaginatedListSkeleton } from './components/home/MoviesPaginatedList/MoviesPaginatedListSkeleton';
+import { SuspenseAfterMounted } from '@/lib/utils/SuspenseAfterMounted';
+import { MountedProvider } from '@/lib/utils/MountedProvider';
 
 export const metadata: Metadata = {
   title: 'ArrowFlicks - Movies',
@@ -14,27 +16,31 @@ export const metadata: Metadata = {
 
 export default function Home(props: { searchParams: { [key: string]: string } }) {
   const { searchParams } = props;
+
   return (
-    <>
-      <PageLayout>
-        <Container w="100%" h="100%" size="1160px" c="black" py="40px" px="90px" bg="grey.2">
-          <Stack h="100%" gap="40px">
-            <Group justify="space-between" wrap="nowrap" gap="40px">
-              <Title size="32px" lh="45px" c="black" order={1} fw="bold">
-                Movies
-              </Title>
-            </Group>
-            <Stack>
-              <Suspense>
-                <SearchFilters searchParams={searchParams} />
-              </Suspense>
-            </Stack>
-            <Suspense key={generateSuspenseKeyBySearchParams(searchParams)} fallback={<MoviesPaginatedListSkeleton />}>
-              <MoviesPaginatedList searchParams={searchParams} />
+    <PageLayout>
+      <Container w="100%" h="100%" size="1160px" c="black" py="40px" px="90px" bg="grey.2">
+        <Stack h="100%" gap="40px">
+          <Group justify="space-between" wrap="nowrap" gap="40px">
+            <Title size="32px" lh="45px" c="black" order={1} fw="bold">
+              Movies
+            </Title>
+          </Group>
+          <Stack>
+            <Suspense>
+              <SearchFilters searchParams={searchParams} />
             </Suspense>
           </Stack>
-        </Container>
-      </PageLayout>
-    </>
+          <MountedProvider>
+            <SuspenseAfterMounted
+              key={generateSuspenseKeyBySearchParams(searchParams)}
+              fallback={<MoviesPaginatedListSkeleton />}
+            >
+              <MoviesPaginatedList searchParams={searchParams} />
+            </SuspenseAfterMounted>
+          </MountedProvider>
+        </Stack>
+      </Container>
+    </PageLayout>
   );
 }
