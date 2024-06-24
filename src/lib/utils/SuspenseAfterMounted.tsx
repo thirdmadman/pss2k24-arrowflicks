c43c17a34eabf7c9f616fa3da382615bcc278700
thirdmadman <thirdmadman@gmail.com>
2024-06-24 11:15:33 +0200

    feat: add SuspenseAfterMounted

---
+++ src/lib/utils/SuspenseAfterMounted.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React, { ReactNode, Suspense, useEffect } from 'react';
+import { useMounted } from './MountedProvider';
+
+export function SuspenseAfterMounted({
+  children,
+  key,
+  fallback,
+}: {
+  children: ReactNode;
+  key: string;
+  fallback: ReactNode;
+}) {
+  const [mounted, setMounted] = useMounted();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return children;
+  }
+
+  return (
+    <Suspense key={key} fallback={fallback}>
+      {children}
+    </Suspense>
+  );
+}
