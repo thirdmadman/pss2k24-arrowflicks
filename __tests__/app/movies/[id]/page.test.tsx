1e1867e2b3568937097c267563c2670ba8c722be
thirdmadman <thirdmadman@gmail.com>
2024-06-24 12:14:48 +0200

    feat: update test

--- __tests__/app/movies/[id]/page.test.tsx
+++ __tests__/app/movies/[id]/page.test.tsx
@@ -1,16 +1,11 @@
-/* eslint-disable max-lines-per-function */
+/* eslint-disable @typescript-eslint/await-thenable */
 import MovieByIdPage from '@/app/movies/[id]/page';
 import { screen } from '@testing-library/react';
 import { render } from '../../../test-utils/render';
-import { getMovieDetailsResponseMock } from '../../../mocks/getMovieDetailsResponseMock';
-import { getMoveVideosResponseMock } from '../../../mocks/getMoveVideosResponseMock';
 
 const mocks = vi.hoisted(() => {
   return {
-    BreadcrumbsNavigation: vi.fn(() => <div>BreadcrumbsNavigation</div>),
-    MovieCardFull: vi.fn(() => <div>MovieCardFull</div>),
-    MovieCardDescription: vi.fn(() => <div>MovieCardDescription</div>),
-    getMoveDetails: vi.fn(() => getMovieDetailsResponseMock),
+    MovieByIdPageContent: vi.fn(() => <div>MovieByIdPageContent</div>),
   };
 });
 
@@ -57,24 +52,8 @@ describe('MovieByIdPage page', () => {
       };
     });
 
-    vi.mock('@/lib/api/getMoveDetails', () => ({
-      getMoveDetails: mocks.getMoveDetails,
-    }));
-
-    vi.mock('@/lib/api/getMoveVideos', () => ({
-      getMoveVideos: () => getMoveVideosResponseMock,
-    }));
-
-    vi.mock('@/app/components/shared/BreadcrumbsNavigation/BreadcrumbsNavigation.tsx', () => ({
-      BreadcrumbsNavigation: mocks.BreadcrumbsNavigation,
-    }));
-
-    vi.mock('@/app/components/movie-id/MovieCardFull/MovieCardFull', () => ({
-      MovieCardFull: mocks.MovieCardFull,
-    }));
-
-    vi.mock('@/app/components/movie-id/MovieCardDescription/MovieCardDescription', () => ({
-      MovieCardDescription: mocks.MovieCardDescription,
+    vi.mock('@/app/movies/[id]/MovieByIdPageContent', () => ({
+      MovieByIdPageContent: mocks.MovieByIdPageContent,
     }));
   });
 
@@ -88,71 +67,6 @@ describe('MovieByIdPage page', () => {
     expect(screen.getByText('ArrowFlicks')).not.toBeNull();
     expect(screen.findAllByTitle('Movies')).not.toBeNull();
 
-    expect(mocks.BreadcrumbsNavigation).toBeCalledWith(
-      {
-        items: [
-          {
-            href: '/',
-            title: 'Movies',
-          },
-          {
-            href: '/movies/0',
-            title: 'Kingdom of the Planet of the Apes',
-          },
-        ],
-      },
-      {}
-    );
-
-    expect(mocks.MovieCardFull).toBeCalledWith(
-      {
-        budget: '$160,000,000',
-        duration: '2h 25m',
-        genres: ['Science Fiction', 'Adventure', 'Action'],
-        grossWorldwide: '$308,839,901',
-        image: {
-          alt: 'Kingdom of the Planet of the Apes',
-          src: 'https://image.tmdb.org/t/p/original/gKkl37BQuKTanygYQG1pyYgLVgf.jpg',
-        },
-        movieId: '0',
-        premiere: 'May 8, 2024',
-        rating: 6.943,
-        reviewsCount: 743,
-        title: 'Kingdom of the Planet of the Apes',
-        year: 2024,
-      },
-      {}
-    );
-
-    expect(mocks.MovieCardDescription).toBeCalledWith(
-      {
-        description:
-          // eslint-disable-next-line prettier/prettier
-          'Several generations in the future following Caesar\'s reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.',
-        productionList: [
-          {
-            imageLink: 'https://image.tmdb.org/t/p/original/h0rjX5vjW5r8yEnUBStFarjcLT4.png',
-            name: '20th Century Studios',
-          },
-          {
-            imageLink: undefined,
-            name: 'Oddball Entertainment',
-          },
-          {
-            imageLink: undefined,
-            name: 'Jason T. Reed Productions',
-          },
-        ],
-        trailerLink: 'https://www.youtube.com/embed/Kdr5oedn7q8',
-      },
-      {}
-    );
-  });
-
-  it('should render error', async () => {
-    mocks.getMoveDetails.mockReturnValueOnce(null as unknown as typeof getMovieDetailsResponseMock);
-    render(await MovieByIdPage({ params: { id: '0' } }));
-    expect(screen.getByText('ArrowFlicks')).not.toBeNull();
-    expect(screen.getByText('Server Error, Please try again Later')).not.toBeNull();
+    expect(mocks.MovieByIdPageContent).toBeCalledWith({ movieId: '0' }, {});
   });
 });
