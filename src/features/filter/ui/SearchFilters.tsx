21f4aef9548af71e4f39efe05d1886b5f62ed799
thirdmadman <thirdmadman@gmail.com>
2024-09-26 19:05:59 +0200

    refactor: separate logic from shared

--- src/features/filter/ui/SearchFilters.tsx
+++ src/features/filter/ui/SearchFilters.tsx
@@ -1,11 +1,11 @@
-import { Flex, Group, Stack, Text } from '@mantine/core';
+import { Flex } from '@mantine/core';
 import { getGenresFetchMap } from '@/lib/api/getGenresFetchMap';
-import { sortByMap } from '@/shared/configs';
-import { InputNumber } from '@/shared/ui/input-number';
-import { MultiSelectInput } from '@/shared/ui/multi-select-input';
-import { SelectInput } from '@/shared/ui/select-input';
+import { GenresSelect } from './genres-select';
+import { RatingFilter } from './rating-filter';
+import { ReleaseYearSelect } from './release-year-select';
 import { ResetFilters } from './reset-filters';
 import classes from './SearchFilters.module.css';
+import { SortBySelect } from './sort-by-select/SortBySelect';
 
 interface ISearchFiltersProps {
   ratingFrom?: string;
@@ -20,61 +20,14 @@ export async function SearchFilters(props: { searchParams: ISearchFiltersProps }
   const searchParams = props?.searchParams || {};
   const { ratingFrom, ratingTo, sortBy, releaseYear, genres } = searchParams;
 
-  const currentYear = new Date().getFullYear();
-  const minYear = 1895;
-  const yearsArray = new Array(currentYear - minYear).fill(1).map((el, i) => String(currentYear - i));
-
   const genresMap = await getGenresFetchMap();
 
   return (
     <Flex className={classes.container}>
-      {genresMap && (
-        <MultiSelectInput
-          queryKey="genres"
-          value={genres}
-          label={'Genres'}
-          options={genresMap}
-          placeholder="Select genre"
-        />
-      )}
-      <SelectInput
-        placeholder="Select release year"
-        queryKey="releaseYear"
-        value={releaseYear}
-        label={'Release year'}
-        options={yearsArray}
-        allowDeselect
-      />
-      <Stack gap="8px" justify="space-between" className={classes.rating}>
-        <Text fw="bold" size="16px" lh="22px">
-          Ratings
-        </Text>
-        <Group wrap="nowrap">
-          <InputNumber
-            min={1}
-            max={10}
-            value={ratingFrom ? parseInt(ratingFrom, 10) : undefined}
-            placeholder="From"
-            queryKey="ratingFrom"
-          />
-          <InputNumber
-            min={1}
-            max={10}
-            value={ratingTo ? parseInt(ratingTo, 10) : undefined}
-            placeholder="To"
-            queryKey="ratingTo"
-          />
-        </Group>
-      </Stack>
-      <SelectInput
-        placeholder="Select sort by"
-        queryKey="sortBy"
-        value={sortBy ?? 'Most popular'}
-        label={'Sort by'}
-        options={sortByMap}
-        defaultValueIndex={0}
-        allowDeselect={false}
-      />
+      {genresMap && <GenresSelect genres={genres} genresMap={genresMap} />}
+      <ReleaseYearSelect releaseYear={releaseYear} />
+      <RatingFilter ratingFrom={ratingFrom} ratingTo={ratingTo} />
+      <SortBySelect sortBy={sortBy} />
       <ResetFilters />
     </Flex>
   );
