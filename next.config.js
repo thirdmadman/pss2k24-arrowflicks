46c67ed9e79b311814f333a8dbe83481d2315060
thirdmadman <thirdmadman@gmail.com>
2025-04-02 23:00:33 +0200

    fix: disable image optimization

--- next.config.js
+++ next.config.js
@@ -4,14 +4,7 @@ const nextConfig = {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
   images: {
-    minimumCacheTTL: 2678400, // 31 days
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**.tmdb.org',
-        port: '',
-      },
-    ],
+    unoptimized: true,
   },
 };
 
