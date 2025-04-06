a81e69ec70226308bfdd9f7975786a4c439cc2ef
thirdmadman <thirdmadman@gmail.com>
2025-04-06 15:55:27 +0200

    feat: add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@mantine/core/styles.css';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import theme from '@/theme/theme';
 import { Inter } from 'next/font/google';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export const metadata = {
   title: 'ArrowFlicks',
@@ -23,6 +24,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="shortcut icon" href="/favicon.ico" />
+
+        <GoogleAnalytics gaId="G-0JQDYRL8H6" />
       </head>
       <body style={{ height: '100%' }}>
         <MantineProvider withGlobalClasses theme={theme}>
